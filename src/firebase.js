f2556fbd1958437a4cdcdca7a26a9fe7b515edba
adiaba roczyno <adiabaroczyno9@gmail.com>
2023-03-08 14:31:52 +0000

    added firebase to for image upload

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC-DBS1O6vsA7-XOav9lB10GBuIGUekF4g",
+  authDomain: "shop-c1208.firebaseapp.com",
+  projectId: "shop-c1208",
+  storageBucket: "shop-c1208.appspot.com",
+  messagingSenderId: "740248685965",
+  appId: "1:740248685965:web:3d1fc82946a41e4aab41c4",
+};
+
+const app = initializeApp(firebaseConfig);
+export default app;
